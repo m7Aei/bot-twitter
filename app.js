de68f5e96e436291cc8e8b1f4fa97c107edf3e66
m7Aei <mateuspm2015@hotmail.com>
2019-07-09 21:48:47 -0300

    Adicionando um metodo para pesquisa de tweets

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const Twit = require('twit')
+const Twit = require("twit");
 
 const T = new Twit({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
+  consumer_key: "",
+  consumer_secret: "",
+  access_token: "",
   access_token_secret: "",
-  timeout_ms: 60 * 1000,
-})
+  timeout_ms: 60 * 1000
+});
 
-if (!process.argv[2]) {
-  console.log('Por favor insira a mensagem do tweet')
+// Pesquisando tweets
+var params = {
+  q: "tweet",
+  count: 5
+};
 
-  return 
+T.get("search/tweets", params, tookData);
+
+function tookData(err, data, response) {
+  var tweets = data.statuses;
+
+  for (var i = 0; i < tweets.lenght; i++) {
+    console.log(tweets[i].text);
+  }
 }
 
-T.post('statuses/update', { status: process.argv[2] }, (err, data, response) => {
-  if (!err) {
-    console.log('Twett publicado!')
+// Postando algum Tweet
+if (!process.argv[2]) {
+  console.log("Por favor insira a mensagem do tweet");
+
+  return;
+}
+
+T.post(
+  "statuses/update",
+  { status: process.argv[2] },
+  (err, data, response) => {
+    if (!err) {
+      console.log("Twett publicado!");
+    }
   }
-})
+);
